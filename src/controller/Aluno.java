8910d8e95fbef2d6a4df39c79bdd87916ebe3ece
leoreis <leoreis@lsr.br>
2019-04-18 20:14:57 -0300

    Adicionado o Projeto Original em Java

---
+++ src/controller/Aluno.java
@@ -0,0 +1,16 @@
+package controller;
+
+import java.sql.Date;
+
+public class Aluno {
+	private int codAluno;
+	private String nomeAluno;
+	private Date nascAluno;
+	private String rgAluno;
+	private String cpfAluno;
+	private String emailAluno;
+	private String teleAluno;
+	private int codResponsavel;
+	
+
+}
